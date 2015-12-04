715ad1066a88456c63ef2f07c420227305bb0ec7
alanknight <alanknight@google.com>
2015-12-03 17:38:02 -0800

    Make Dart Intl message extraction force --warnings-are-errors ------------- Created by MOE: https://github.com/google/moe MOE_MIGRATED_REVID=109365650

--- lib/src/intl_message.dart
+++ lib/src/intl_message.dart
@@ -118,7 +118,7 @@ abstract class Message {
         : "${classDeclaration.name.token.toString()}_$outerName";
     var classMatch = classPlusMethod != null && (givenName == classPlusMethod);
     if (!(hasOuterName && (simpleMatch || classMatch))) {
-      return "The 'name' argument for Intl.message must match either"
+      return "The 'name' argument for Intl.message must match either "
           "the name of the containing function or <className>_<methodName> ("
           "'$givenName' vs. '$outerName')";
     }
