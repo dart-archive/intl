67ae354f1430062ce966051bb994ae105d9d118d
kevmoo <kevmoo@google.com>
2020-10-06 00:31:09 +0000

    Remove gratuitous self-link in main library
    Also add missing `dart` definition in readme code block
    
    PiperOrigin-RevId: 335536174

--- lib/intl.dart
+++ lib/intl.dart
@@ -6,9 +6,6 @@
 /// message formatting and replacement, date and number formatting and parsing,
 /// and utilities for working with Bidirectional text.
 ///
-/// This is part of the [intl package]
-/// (https://pub.dartlang.org/packages/intl).
-///
 /// For things that require locale or other data, there are multiple different
 /// ways of making that data available, which may require importing different
 /// libraries. See the class comments for more details.
