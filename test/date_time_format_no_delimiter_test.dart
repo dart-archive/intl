70659c02befa8c2efbe0f4e78408298322c1eeda
Jhionan <jhionan@gmail.com>
2022-08-18 16:59:51 -0300

    test: update test cases

--- test/date_time_format_no_delimiter_test.dart
+++ test/date_time_format_no_delimiter_test.dart
@@ -10,6 +10,11 @@ void main() {
     final result = DateFormat('yyyyMMddHHmmss').parseNoDelimiter(timestamp);
     expect(result, DateTime(2019, 06, 14, 07, 19, 30));
   });
+   test('No delimiter 3 digits year', () {
+    final timestamp = "20190614071930";
+    final result = DateFormat('yyyMMddHHmmss').parseNoDelimiter(timestamp);
+    expect(result, DateTime(2019, 06, 14, 07, 19, 30));
+  });
   
   test('No delimiter 2 digits year', () {
     final timestamp = "190614071930";
@@ -19,16 +24,22 @@ void main() {
   test('No delimiter 1 digits year', () {
     final timestamp = "190614071930";
     final result = DateFormat('yMMddHHmmss').parseNoDelimiter(timestamp);
-    expect(result, DateTime(0019, 06, 14, 07, 19, 30));
+    expect(result, DateTime(19, 06, 14, 07, 19, 30));
+  });
+
+  test('No delimiter n digits year', () {
+    final timestamp = "20190614071930";
+    final result = DateFormat('yyyyyyMMddHHmmss').parseNoDelimiter(timestamp);
+    expect(result, DateTime(2019, 06, 14, 07, 19, 30));
   });
   test('No delimiter 2 digits year with other order', () {
     final timestamp = "061407193019";
     final result = DateFormat('MMddHHmmssyy').parseNoDelimiter(timestamp);
     expect(result, DateTime(2019, 06, 14, 07, 19, 30));
   });
-  test('No delimiter 1 digits year', () {
-    final timestamp = "061419071930";
-    final result = DateFormat('MMddyHHmmss').parseNoDelimiter(timestamp);
+  test('No delimiter 1 digits year random order', () {
+    final timestamp = "140619071930";
+    final result = DateFormat('ddMMyHHmmss').parseNoDelimiter(timestamp);
     expect(result, DateTime(0019, 06, 14, 07, 19, 30));
   });
 
@@ -38,5 +49,16 @@ void main() {
     final result = DateFormat('MMddHHmmssyyyy').parseNoDelimiter(timestamp);
     expect(result, DateTime(2019, 06, 14, 07, 19, 30));
   });
+   test('With delimiter 4 digits year other order', () {
+    final timestamp = "06-14-07-19-30-2019";
+    final result = DateFormat('MM-dd-HH-mm-ss-yyyy').parseNoDelimiter(timestamp);
+    expect(result, DateTime(2019, 06, 14, 07, 19, 30));
+  });
+   test('With delimiter 3 digits year other order', () {
+    final timestamp = "06-14-07-19-30-2019";
+    final result = DateFormat('MM-dd-HH-mm-ss-yyy').parseNoDelimiter(timestamp);
+    expect(result, DateTime(2019, 06, 14, 07, 19, 30));
+  });
+   
   
 }
